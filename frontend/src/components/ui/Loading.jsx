bf31c8dfdd9c255ac46764e44ba3617d76c1b9a3
정민영 <myjeong19@naver.com>
2024-03-09 20:50:35 +0900

    Stlye. Loading

---
+++ frontend/src/components/ui/Loading.jsx
@@ -0,0 +1,20 @@
+import './css/Loading.scss';
+
+const Loading = () => {
+  return (
+    <main className='main'>
+      <div className='spinner'>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+
+      <h1>Loading ...</h1>
+    </main>
+  );
+};
+
+export default Loading;
