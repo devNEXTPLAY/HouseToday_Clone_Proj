e847fc79a01aa432003ea8b3aba476328e717777
정민영 <myjeong19@naver.com>
2024-03-21 03:10:19 +0900

    Feat. 약관 전체 동의 및 개별 동의

---
+++ frontend/src/components/ui/AgreeInput.jsx
@@ -0,0 +1,13 @@
+function AgreeInput({ id, title, condition, checked, ...props }) {
+  return (
+    <div className='agree__chackbox'>
+      <input {...props} id={id} checked={checked} type='checkbox' />
+      <label htmlFor={id}>
+        {title && title}
+        <span>({condition})</span>
+      </label>
+    </div>
+  );
+}
+
+export default AgreeInput;
--- frontend/src/pages/auth/Signup.jsx
+++ frontend/src/pages/auth/Signup.jsx
@@ -1,3 +1,5 @@
+import { v4 as uuidv4 } from 'uuid';
+
 import './css/Signup.scss';
 import { Link, useNavigate } from 'react-router-dom';
 import { useState } from 'react';
@@ -6,13 +8,83 @@ import axios from 'axios';
 import SnsLogin from '../../components/widgets/SnsLogin';
 import Input from '../../components/ui/Input';
 import Button from '../../components/ui/Button';
+import AgreeInput from '../../components/ui/AgreeInput';
+
+// 동의 항목 리스트
+const agreeList = [
+  {
+    state: 'age',
+    checked: false,
+    title: '만 14세이상입니다',
+    condition: '필수',
+  },
+  {
+    state: 'service',
+    checked: false,
+    title: '이용약관',
+    condition: '필수',
+  },
+  {
+    state: 'privacy',
+    checked: false,
+    title: '개인정보 수집 및 이용동의',
+    condition: '필수',
+  },
+  {
+    state: 'agree_promotion',
+    checked: false,
+    title: '개인정보 마케팅 활용 동의',
+    condition: '선택',
+  },
+  {
+    state: 'agree_promotion',
+    checked: false,
+    title: '이벤트, 쿠폰, 특가, 알림 메일 및 SMS 수신',
+    condition: '선택',
+  },
+];
 
 // * 회원가입
 const Signup = () => {
-  const [isAllChecked, setIsAllChecked] = useState(false);
+  const [isAgreeAll, setIsAgreeAll] = useState(false);
+  const [isAgree, setIsAgree] = useState(agreeList);
 
-  const handleIsAllCheck = () => {
-    setIsAllChecked(prevState => !prevState);
+  // * 전체 동의 핸들러
+  const handleIsAgreeAll = () => {
+    if (isAgreeAll === false) {
+      setIsAgreeAll(true);
+      setIsAgree(prevIsAgree => {
+        const updateIsAgree = prevIsAgree.map(agree => {
+          return { ...agree, checked: true };
+        });
+
+        return updateIsAgree;
+      });
+    } else {
+      setIsAgreeAll(false);
+      setIsAgree(prevIsAgree => {
+        const updateIsAgree = prevIsAgree.map(agree => {
+          return { ...agree, checked: false };
+        });
+
+        return updateIsAgree;
+      });
+    }
+  };
+
+  // * 개별 동의 핸들러
+  const handleIsAgree = agree => {
+    setIsAgree(prevIsAgree => {
+      const updateIsAgree = prevIsAgree.map(item => {
+        if (item.title === agree.title) {
+          return { ...item, checked: !item.checked };
+        }
+        return item;
+      });
+      return updateIsAgree;
+    });
+
+    console.log(isAgree);
   };
 
   const navigate = useNavigate();
@@ -115,55 +187,34 @@ const Signup = () => {
           onChange={onRegistChange}
         />
 
-        {/* // Todo 체크박스 스타일링 및 전체 선택 기능 */}
         <section className='agree-section'>
-          <div className='agree__chackbox'>
-            <input
-              type='checkbox'
-              id='agree_all'
-              name='agree_all'
-              onClick={handleIsAllCheck}
-            />
-            <label htmlFor='agree_all'>
-              <strong>전체 동의</strong> <span>선택항목에 대한 동의 포함</span>
-            </label>
-          </div>
-          <hr />
+          <h4>약관 동의</h4>
+          <div className='section__container'>
+            <div className='agree__chackbox'>
+              <input
+                type='checkbox'
+                id='agree_all'
+                name='agree_all'
+                onChange={handleIsAgreeAll}
+              />
+              <label htmlFor='all'>
+                <strong>전체 동의</strong>{' '}
+                <span>선택항목에 대한 동의 포함</span>
+              </label>
+            </div>
 
-          <div className='agree__chackbox'>
-            <input
-              type='checkbox'
-              id='agree_age'
-              name='agree_age'
-              checked={isAllChecked}
-            />
-            <label htmlFor='agree_age'>
-              만14세이상입니다 <span>(필수)</span>
-            </label>
-          </div>
+            <hr />
 
-          <div className='agree__chackbox'>
-            <input
-              type='checkbox'
-              id='agree_promotion'
-              name='agree_promotion'
-              checked={isAllChecked}
-            />
-            <label htmlFor='agree_promotion'>
-              개인정보수집 및 이용 동의 <span>(필수)</span>
-            </label>
-          </div>
-          <div className='agree__chackbox'>
-            <input
-              type='checkbox'
-              id='agree_marketing'
-              name='agree_marketing'
-              checked={isAllChecked}
-            />
-
-            <label htmlFor='agree_marketing'>
-              마케팅 정보 수신 동의 <span>(선택)</span>
-            </label>
+            {isAgree.map(agree => (
+              <AgreeInput
+                key={agree.title}
+                id={agree.state}
+                title={agree.title}
+                condition={agree.condition}
+                checked={agree.checked}
+                onChange={() => handleIsAgree(agree)}
+              />
+            ))}
           </div>
         </section>
 
