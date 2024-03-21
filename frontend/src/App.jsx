a663828e7da13eb14a71f9ba5214c61ebcc74b9f
Eunbizz <eb001020@naver.com>
2024-03-21 17:05:24 +0900

    App.js 수정전

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,69 +1,73 @@
-import { lazy } from 'react';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import HomeLayout from './layout/HomeLayout';
+import { lazy } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+// Layouts
+import HomeLayout from "./layout/HomeLayout";
+import NonAuthHomeLayout from "./layout/NonAuthHomeLayout";
 
 // ! 사용자 인증 관련 화면
-const Login = lazy(() => import('./pages/auth/Login'));
-const Signup = lazy(() => import('./pages/auth/Signup'));
+const Login = lazy(() => import("./pages/auth/Login"));
+const Signup = lazy(() => import("./pages/auth/Signup"));
 
 // ! 게시글 설정 관련 화면
-const Community = lazy(() => import('./pages/community/Community'));
-const Write = lazy(() => import('./pages/community/Write'));
-const WriteEdit = lazy(() => import('./pages/community/WriteEdit'));
-const Post = lazy(() => import('./pages/community/Post'));
+const Community = lazy(() => import("./pages/community/Community"));
+const Write = lazy(() => import("./pages/community/Write"));
+const WriteEdit = lazy(() => import("./pages/community/WriteEdit"));
+const Post = lazy(() => import("./pages/community/Post"));
 
 const HousewraimngParty = lazy(() =>
-  import('./pages/housewarming-party/HousewarmingParty')
+  import("./pages/housewarming-party/HousewarmingParty")
 );
 
-const HousePhoto = lazy(() => import('./pages/house-photo/HousePhoto'));
+const HousePhoto = lazy(() => import("./pages/house-photo/HousePhoto"));
 
 // ! 사용자 설정 관련 화면
-const User = lazy(() => import('./pages/user/User'));
-const Setting = lazy(() => import('./pages/user/Setting'));
+const User = lazy(() => import("./pages/user/User"));
+const Setting = lazy(() => import("./pages/user/Setting"));
 
 // ! 홈 화면
-const Home = lazy(() => import('./pages/Home'));
+const Home = lazy(() => import("./pages/Home"));
 
-const Loading = lazy(() => import('./components/ui/Loading'));
+const Loading = lazy(() => import("./components/ui/Loading"));
 
 const router = createBrowserRouter([
   // ! 사용자 인증 관련 화면
   // * 로그인 화면 http://localhost:5173/login
-  { path: '/login', element: <Login /> },
+  { path: "/login", element: <Login /> },
   // * 회원가입 화면 http://localhost:5173/signup
-  { path: '/signup', element: <Signup /> },
+  { path: "/signup", element: <Signup /> },
 
   // ! 홈 화면
   {
-    index: '/',
+    index: "/",
     element: <HomeLayout />,
     children: [
       // * 홈 화면 http://localhost:5173/
       { index: true, element: <Home /> },
       // * 집들이 화면 http://localhost:5173/housewarming_party
-      { path: 'housewarming_party', element: <HousewraimngParty /> },
+      { path: "housewarming_party", element: <HousewraimngParty /> },
       // * 집 사진 화면 http://localhost:5173/house_photo
-      { path: 'house_photo', element: <HousePhoto /> },
+      { path: "house_photo", element: <HousePhoto /> },
       // * 글 목록 화면 http://localhost:5173/comunity
-      { path: 'community', element: <Community /> },
+      { path: "community", element: <Community /> },
     ],
   },
 
   // ! 게시글 관련
   // * 글쓰기 화면 http://localhost:5173/write
-  { path: '/write', element: <Write /> },
+  { path: "/write", element: <Write /> },
   // * 글 수정 화면 http://localhost:5173/write/edit/:aid
-  { path: '/write/edit/:aid', element: <WriteEdit /> },
+  { path: "/write/edit/:aid", element: <WriteEdit /> },
 
   // * 글 읽기 화면 http://localhost:5173/post/:aid
-  { path: '/post/:aid', element: <Post /> },
+  { path: "/post/:aid", element: <Post /> },
 
   // ! 사용자 설정 관련 화면
   // * 마이페이지 화면 http://localhost:5173/users/:uid
-  { path: '/users/:uid', element: <User /> },
+  { path: "/users/:uid", element: <User /> },
   // * 설정 화면 http://localhost:5173/users/:uid/push
-  { path: '/users/:uid/edit', element: <Setting /> },
+  { path: "/users/:uid/edit", element: <Setting /> },
 ]);
 
 function App() {
---
+++ frontend/src/components/widgets/NonAuthHomeNavigation.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+import "./css/HomeNavigation.scss";
+
+import Input from "../ui/Input";
+import Button from "../ui/Button";
+
+import { TextLogo } from "../../assets/TextLogo";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { CiBellOn } from "react-icons/ci";
+
+// * 헤더 네비게이션
+const NonAuthHomeNavigation = () => {
+  return (
+    <header className="main-header">
+      <button className="hamburger">
+        {/* //* 햄버거 아이콘 */}
+        <RxHamburgerMenu />
+      </button>
+
+      <div className="header__container">
+        <Link to="/">
+          {/* //* 오늘의 집 로고 */}
+          <TextLogo />
+        </Link>
+
+        <section className="container__section">
+          <Input placeholder="게시물 검색" custom="search" />
+
+          <div className="header__auth">
+            <Link to="/login">로그인</Link>
+            <Link to="/signup" className="link__margin">
+              회원가입
+            </Link>
+          </div>
+
+          {/* //* 글쓰기 이동 링크 */}
+          <Link to="/write">
+            <Button>글쓰기</Button>
+          </Link>
+        </section>
+      </div>
+    </header>
+  );
+};
+
+export default NonAuthHomeNavigation;
