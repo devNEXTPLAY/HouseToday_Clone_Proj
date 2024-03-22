7c135a5e9fd399f8d37179ff86d909a7ee194e7b
정민영 <myjeong19@naver.com>
2024-03-22 22:23:39 +0900

    Feat. 이미지 사이즈 압축, 해시태그 DB name -> hashtag_name 수정

--- backend/models/blogs/hashtags.js
+++ backend/models/blogs/hashtags.js
@@ -1,32 +1,32 @@
 module.exports = function (sequelize, DataTypes) {
-	return sequelize.define(
-		"Hashtag",
-		{
-			hashtag_id: {
-				type: DataTypes.INTEGER,
-				primaryKey: true,
-				autoIncrement: true,
-				allowNull: false,
-				comment: "해시태그 고유번호",
-			},
-			name: {
-				type: DataTypes.STRING(100),
-				allowNull: false,
-				unique: true,
-				comment: "해시태그 이름",
-			},
-			click_count: {
-				type: DataTypes.INTEGER,
-				allowNull: false,
-				defaultValue: 0,
-				comment: "해시태그 클릭 횟수",
-			},
-		},
-		{
-			sequelize,
-			timestamps: false,
-			comment: "해시태그 테이블",
-			tableName: "Hashtags",
-		}
-	);
+  return sequelize.define(
+    "Hashtag",
+    {
+      hashtag_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+        comment: "해시태그 고유번호",
+      },
+      hashtag_name: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+        unique: true,
+        comment: "해시태그 이름",
+      },
+      click_count: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+        comment: "해시태그 클릭 횟수",
+      },
+    },
+    {
+      sequelize,
+      timestamps: false,
+      comment: "해시태그 테이블",
+      tableName: "Hashtags",
+    }
+  );
 };
