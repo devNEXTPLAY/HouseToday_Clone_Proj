7a7413d53af87a93d7d0aaa96d45b94b4ee1f03a
Eunbizz <eb001020@naver.com>
2024-03-26 16:46:20 +0900

    kakao login

--- frontend/src/components/widgets/SnsLogin.jsx
+++ frontend/src/components/widgets/SnsLogin.jsx
@@ -1,6 +1,7 @@
 import "./css/SnsLogin.scss";
 
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 // * SVG 아이콘 참조
 import { Facebook } from "../../assets/Facebook.jsx";
@@ -10,15 +11,12 @@ import { Naver } from "../../assets/Naver.jsx";
 // * SNS 로그인
 //* props text: text 로고 상단에 표시할 텍스트
 const SnsLogin = ({ text, children }) => {
-  const Rest_api_key = import.meta.env.VITE_REST_API_KEY;
-  const redirect_uri = "http://localhost:5173/oauth";
-  const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${Rest_api_key}&redirect_uri=${redirect_uri}&response_type=code`;
   const handleKakao = () => {
-    window.location.href = kakaoURL;
-  };
-  // 인가코드 추출
-  const code = new URL(window.location.href).searchParams.get("code");
-  console.log(code);
+    axios.get("http://localhost:3005/api/users/kakao").then((res)=>{
+      console.log(res);
+    }).catch();
+  }
+  
 
   return (
     <section className="login-box">
