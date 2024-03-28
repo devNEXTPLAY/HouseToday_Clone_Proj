6ccd40fbe5b7406f258726279216b57f33d452b4
Hyo1-Lee <gydnjsdl426@naver.com>
2024-03-28 11:53:17 +0900

    추천 블로그 fetch 코드 디버그

--- backend/public/js/getRecommendedBlogs.js
+++ backend/public/js/getRecommendedBlogs.js
@@ -3,38 +3,39 @@
 // 8개의 블로그를 추천하는 함수
 const db = require("../../models");
 const enums = require("../../common/enums.js");
+const { Op } = require("sequelize");
 
 async function getRecommendedBlogs() {
-    try {
-        const blogs = await db.Blogs.findAll({
-            attributes: ["blog_id", "view_count", "like_count", "comment_count", "reg_date"],
-            where: {
-                blog_status_code: enums.BLOG_STATUS_CODE.APPROVED,
-            },
-        });
+	try {
+		const blogs = await db.Blogs.findAll({
+			attributes: ["blog_id", "view_count", "like_count", "comment_count", "reg_date"],
+			where: {
+				blog_status_code: {
+					[Op.or]: [enums.BLOG_STATUS_CODE.APPROVED, enums.BLOG_STATUS_CODE.APPLIED],
+				},
+			},
+		});
 
-        const blogScores = blogs.map((blog) => {
-            const hoursSincePosted = (new Date() - new Date(blog.reg_date)) / (1000 * 60 * 60);
-            let timeScore = hoursSincePosted <= 24 ? 1 : 1 / Math.pow(hoursSincePosted - 23, 2);
-            const popularityScore =
-                Math.log(
-                    1 + blog.view_count * 0.1 + blog.like_count * 0.6 + blog.comment_count * 0.3
-                ) / Math.log(5);
-            const score = timeScore * 0.8 + popularityScore * 0.2;
+		const blogScores = blogs.map((blog) => {
+			const hoursSincePosted = (new Date() - new Date(blog.reg_date)) / (1000 * 60 * 60);
+			let timeScore = hoursSincePosted <= 24 ? 1 : 1 / Math.pow(hoursSincePosted - 23, 2);
+			const popularityScore =
+				Math.log(1 + blog.view_count * 0.1 + blog.like_count * 0.6 + blog.comment_count * 0.3) / Math.log(5);
+			const score = timeScore * 0.8 + popularityScore * 0.2;
 
-            return { blog_id: blog.blog_id, score };
-        });
+			return { blog_id: blog.blog_id, score };
+		});
 
-        const recommendedBlogs = blogScores
-            .sort((a, b) => b.score - a.score)
-            .slice(0, 8)
-            .map((blog) => blog.blog_id);
+		const recommendedBlogs = blogScores
+			.sort((a, b) => b.score - a.score)
+			.slice(0, 8)
+			.map((blog) => blog.blog_id);
 
-        return recommendedBlogs;
-    } catch (error) {
-        console.error(error);
-        throw error;
-    }
+		return recommendedBlogs;
+	} catch (error) {
+		console.error(error);
+		throw error;
+	}
 }
 
 module.exports = getRecommendedBlogs;
