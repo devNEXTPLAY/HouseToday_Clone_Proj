fa129bda31f47e1c8b660599b206f3fac8248a63
Eunbizz <eb001020@naver.com>
2024-04-01 14:05:15 +0900

    Feat. 검색창

--- frontend/src/components/home/navigation/SearchInput.jsx
+++ frontend/src/components/home/navigation/SearchInput.jsx
@@ -1,15 +1,24 @@
 import { IoIosSearch } from 'react-icons/io'
+import { useNavigate } from 'react-router-dom'
 
 import classes from './css/SearchInput.module.css'
 
 const SearchInput = ({ ...props }) => {
+    const navigate = useNavigate()
+
+    const handleKeyDown = (e) => {
+        if (e.key === 'Enter') {
+            navigate(`/search?keyword=${e.target.value}`)
+        }
+    }
+
     return (
         <div className={classes.search}>
             <span>
                 <IoIosSearch size="24" color="#2F3438" />
             </span>
 
-            <input {...props} placeholder="통합 검색" />
+            <input {...props} onKeyDown={handleKeyDown} placeholder="통합 검색" />
         </div>
     )
 }
